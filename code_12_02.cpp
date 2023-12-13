42b8e2f37056a0547d4448f8e45b07883504f00f
KShih <katetino@gmail.com>
2023-12-12 20:30:02 -0500

    Day 12 pt 2

--- code_12_02.cpp
+++ code_12_02.cpp
@@ -3,8 +3,19 @@
 #include <sstream>
 #include <vector>
 #include <regex>
+#include <unordered_map>
 
-int get_arrangements(std::string blueprint, std::vector<int> counts, int cidx){
+long long get_arrangements(std::string blueprint, std::vector<int> counts, int cidx, std::unordered_map<std::string, long long> &cached){
+    std::string map_key = blueprint + "|";
+    for(int i : counts){
+        map_key += std::to_string(i) + "|";
+    }
+    map_key += std::to_string(cidx);
+
+    if(cached.find(map_key) != cached.end()){
+        return cached[map_key];
+    }
+
     if((cidx >= counts.size())){ // no more groups to match
         if(blueprint.find('#') >= blueprint.size()){ // no more hashes in sequence
             return 1;
@@ -20,16 +31,16 @@ int get_arrangements(std::string blueprint, std::vector<int> counts, int cidx){
     else{
         if(blueprint[0] == '.'){ // leading '.' can be ignored
             //std::cout << "cut off lone . \n";
-            return get_arrangements(blueprint.substr(1), counts, cidx);
+            return get_arrangements(blueprint.substr(1), counts, cidx, cached);
         }
         else if(blueprint[0] == '#'){ // check if leading '#' corresponds to first group remaining
             if((blueprint.substr(0, counts.at(cidx)).find('.') >= counts.at(cidx)) && (blueprint[counts.at(cidx)] != '#')){ // the sequence of '#' and '?' is long enough, and there's space for a '.' afterwards
                 //std::cout << "found group, chopping off hashes \n";
                 if(counts.at(cidx) >= blueprint.size()){
-                    return get_arrangements(std::string(""), counts, cidx + 1);
+                    return get_arrangements(std::string(""), counts, cidx + 1, cached);
                 }
                 else{
-                    return get_arrangements(blueprint.substr(counts.at(cidx) + 1), counts, cidx + 1);
+                    return get_arrangements(blueprint.substr(counts.at(cidx) + 1), counts, cidx + 1, cached);
                 }
             }
             else{
@@ -39,11 +50,13 @@ int get_arrangements(std::string blueprint, std::vector<int> counts, int cidx){
         }
         else{
             //std::cout << "forking \n";
-            int arr = 0;
+            long long arr = 0;
             blueprint[0] = '#';
-            arr += get_arrangements(blueprint, counts, cidx);
+            arr += get_arrangements(blueprint, counts, cidx, cached);
             //std::cout << "(fork here)\n";
-            arr += get_arrangements(blueprint.substr(1), counts, cidx);
+            arr += get_arrangements(blueprint.substr(1), counts, cidx, cached);
+
+            cached[map_key] = arr;
             return arr;
         }
     }
@@ -53,8 +66,7 @@ int get_arrangements(std::string blueprint, std::vector<int> counts, int cidx){
 int main(){
     std::ifstream input_file("./Inputs/input_12.txt");
 
-    int sum = 0;
-    int ii = 1;
+    long long sum = 0;
 
     for(std::string line; std::getline(input_file, line);){
         int split = line.find(' ');
@@ -76,12 +88,12 @@ int main(){
             }
         }
 
-        int arr = get_arrangements(blueprint_long, counts_long, 0);
+        std::unordered_map<std::string, long long> cache;
+        long long arr = get_arrangements(blueprint_long, counts_long, 0, cache);
 
-        std::cout<< ii << " " << blueprint_long << " " << arr <<  "\n";
+        //std::cout << blueprint_long << " " << arr <<  "\n";
 
         sum += arr;
-        ii += 1;
     }
 
 std::cout << sum << "\n";
